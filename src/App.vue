e6c04c7c5aad469bc25d8b413bb8280d3989e8a3
spark <parkyngj@gmail.com>
2020-03-04 16:11:04 -0800

    upload all assets to app

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <section class="header">
       <div class="columns has-text-centered">
         <div class="column is-one-third is-offset-one-third">
-          <img src="https://www.stickpng.com/assets/images/5c80f70f72f5d9028c17ed26.png">
+          <img src="./assets/header-at-logo.png">
           <br/>
           <h1 class="title">Adventure Time Wisdom</h1>
           <h1 class="subtitle">Inspirational, insightful <s>nonsense</s> quotes from the widely beloved children's show, Adventure Time.</h1>
@@ -76,7 +76,7 @@ export default {
 @import '../node_modules/bulma/css/bulma.css';
 
 html {
-  background-image: url("https://i.imgur.com/RETkwA5.png");
+  background-image: url("./assets/bg-chars-breakfasts.png");
   background-color: rgba(255, 255, 255, 0.8);
   background-blend-mode: lighten;
 }
